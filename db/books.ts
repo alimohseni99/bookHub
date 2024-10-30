d28d0a543f11fc5737e566ddf5e5b54f28eb65f5
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-30 22:18:00 +0100

    created express app that shows hello world on localhost:3000

--- db/books.ts
+++ db/books.ts
@@ -1,72 +1,72 @@
-const books = [
+export const books = [
   {
     id: 1,
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
     publishedYear: 1960,
-    genre: "Fiction"
+    genre: "Fiction",
   },
   {
     id: 2,
     title: "1984",
     author: "George Orwell",
     publishedYear: 1949,
-    genre: "Dystopian"
+    genre: "Dystopian",
   },
   {
     id: 3,
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     publishedYear: 1925,
-    genre: "Tragedy"
+    genre: "Tragedy",
   },
   {
     id: 4,
     title: "Pride and Prejudice",
     author: "Jane Austen",
     publishedYear: 1813,
-    genre: "Romance"
+    genre: "Romance",
   },
   {
     id: 5,
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
     publishedYear: 1951,
-    genre: "Fiction"
+    genre: "Fiction",
   },
   {
     id: 6,
     title: "The Lord of the Rings",
     author: "J.R.R. Tolkien",
     publishedYear: 1954,
-    genre: "Fantasy"
+    genre: "Fantasy",
   },
   {
     id: 7,
     title: "Moby-Dick",
     author: "Herman Melville",
     publishedYear: 1851,
-    genre: "Adventure"
+    genre: "Adventure",
   },
   {
     id: 8,
     title: "War and Peace",
     author: "Leo Tolstoy",
     publishedYear: 1869,
-    genre: "Historical Fiction"
+    genre: "Historical Fiction",
   },
   {
     id: 9,
     title: "The Odyssey",
     author: "Homer",
     publishedYear: -800,
-    genre: "Epic"
+    genre: "Epic",
   },
   {
     id: 10,
     title: "Ulysses",
     author: "James Joyce",
     publishedYear: 1922,
-    genre: "Modernist"
-  }
+    genre: "Modernist",
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import express from "express";
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000);
